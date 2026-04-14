3b61702b3a3fc9ef2663a680e2ed70979846ff97
Harshvardhan <harshvardhanrokade845@gmail.com>
2026-04-14 08:25:22 +0530

    Postgres and Redis initial setup

--- auth-service/src/main/java/com/jamify/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/jamify/auth/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.jamify.auth_service;
+package com.jamify.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- auth-service/src/test/java/com/jamify/auth/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/jamify/auth/AuthServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jamify.auth_service;
+package com.jamify.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
